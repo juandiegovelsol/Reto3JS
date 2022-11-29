34e4874a35e8cfb571c990924daa4b6e67e3040f
Juan <juandiegovelsol@gmail.com>
2022-11-28 19:53:04 -0500

    Solucion Reto

---
+++ Reto3JS.js
@@ -0,0 +1,15 @@
+console.log(myFunction(["a", "b", "c"], [1, 2, 3]));
+console.log(myFunction(["a", "b", "c"], [1, () => {}, { name: "khriztian" }]));
+console.log(
+  myFunction(
+    ["name", "hobbies", "isAdmin"],
+    ["khriztian", ["music", "tv series"], false]
+  )
+);
+function myFunction(a, b) {
+  const obj = {};
+  a.forEach((element, index) => {
+    obj[element] = b[index].toString();
+  });
+  return obj;
+}
